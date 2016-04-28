2f462a6582e1382b9dcb5772fa51e89d837c95f6
Marwan <eng.marawan1988@yahoo.com>
2016-04-28 14:22:08 +0200

    JDatepicker component

--- src/com/jasperreport/shiro/ShiroMainTest.java
+++ src/com/jasperreport/shiro/ShiroMainTest.java
@@ -1,90 +1,91 @@
 package com.jasperreport.shiro;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Properties;
 
-import javax.swing.JButton;
 import javax.swing.JFormattedTextField.AbstractFormatter;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 
 public class ShiroMainTest {
 
 	public static void main(String... arg) {
 
-		
-		// create object of JLabel and set label
-		JLabel label = new JLabel("Selected Date:");
-		// create object of JTextField and declare it final
-		final JTextField text = new JTextField(20);
-		// create object of JButton
-		JButton b = new JButton("popup");
-		// create object of JPanel
-		JPanel p = new JPanel();
-		p.add(label);
-		p.add(text);
-		p.add(b);
-
-		final JFrame f = new JFrame();
-		f.getContentPane().add(p);
-		f.pack();
-		f.setVisible(true);
-		// add action listener
-		b.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				// set text i.e. date
-				text.setText(new DatePicker(f).setPickedDate());
-			}
-		});
-		JButton button = new JButton("get Date");
-		p.add(button);
-		button.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-				String txt = text.getText();
-				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-				
-				System.out.println(text.getText());
-				try {
-					System.out.println(format.parse(txt));
-				} catch (ParseException e1) {
-					e1.printStackTrace();
-				}
-			}
-		});
-
-		// DecimalFormat df = new DecimalFormat("#.##");
-		// df.setRoundingMode(RoundingMode.CEILING);
-		// String x = df.format(-12.877883);
-		// System.out.println(Double.valueOf(x));
-		// x = df.format(-12.877883);
-		// System.out.println(Double.valueOf(x));
+		// // create object of JLabel and set label
+		// JLabel label = new JLabel("Selected Date:");
+		// // create object of JTextField and declare it final
+		// final JTextField text = new JTextField(20);
+		// // create object of JButton
+		// JButton b = new JButton("popup");
+		// // create object of JPanel
+		// JPanel p = new JPanel();
+		// p.add(label);
+		// p.add(text);
+		// p.add(b);
 		//
-		// JFrame frame = new JFrame();
-		// frame.setSize(300, 300);
-		// UtilDateModel model = new UtilDateModel();
-		// // model.setDate(20,04,2014);
-		// // Need this...
-		// Properties p = new Properties();
-		// p.put("text.today", "Today");
-		// p.put("text.month", "Month");
-		// p.put("text.year", "Year");
-		// JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+		// final JFrame f = new JFrame();
+		// f.getContentPane().add(p);
+		// f.pack();
+		// f.setVisible(true);
+		// // add action listener
+		// b.addActionListener(new ActionListener() {
+		// public void actionPerformed(ActionEvent ae) {
+		// // set text i.e. date
+		// text.setText(new DatePicker(f).setPickedDate());
+		// }
+		// });
+		// JButton button = new JButton("get Date");
+		// p.add(button);
+		// button.addActionListener(new ActionListener() {
 		//
-		// // Don't know about the formatter, but there it is...
-		// JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new
-		// DateLabelFormatter());
+		// @Override
+		// public void actionPerformed(ActionEvent e) {
 		//
-		// frame.getContentPane().add(datePicker);
-		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// frame.setVisible(true);
+		// String txt = text.getText();
+		// SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		//
+		// System.out.println(text.getText());
+		// try {
+		// System.out.println(format.parse(txt));
+		// } catch (ParseException e1) {
+		// e1.printStackTrace();
+		// }
+		// }
+		// });
+
+		/*
+		 * DecimalFormat df = new DecimalFormat("#.##");
+		 * df.setRoundingMode(RoundingMode.CEILING); String x =
+		 * df.format(-12.877883); System.out.println(Double.valueOf(x)); x =
+		 * df.format(-12.877883); System.out.println(Double.valueOf(x));
+		 */
+
+		JFrame frame = new JFrame();
+		frame.setSize(300, 300);
+		UtilDateModel model = new UtilDateModel();
+		// model.setDate(20,04,2014);
+		// Need this...
+
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+
+		// Don't know about the formatter, but there it is...
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+		frame.setLocationRelativeTo(null);
+		JPanel panel = new JPanel();
+		panel.add(datePicker);
+		frame.getContentPane().add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 	public static class DateLabelFormatter extends AbstractFormatter {
